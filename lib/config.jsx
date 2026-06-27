168c8005b5069db8f2704937ddf7ddf8387ffc36
xuantien1026 <xuantien1026@gmail.com>
2026-06-27 12:50:58 +0700

    Add size 13

--- lib/config.jsx
+++ lib/config.jsx
@@ -1,4 +1,4 @@
 // Shared configuration
 
 // Garment sizes, smallest to largest.
-var SIZES = ['1', '3', '5', '7', '9', '11', 'XS', 'S', 'M', 'L', 'XL', '2XL', '3XL', '4XL', '5XL', '6XL'];
+var SIZES = ['1', '3', '5', '7', '9', '11', '13', 'XS', 'S', 'M', 'L', 'XL', '2XL', '3XL', '4XL', '5XL', '6XL'];
